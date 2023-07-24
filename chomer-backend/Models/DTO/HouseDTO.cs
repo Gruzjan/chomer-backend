d412c44a6df5489767f18d8835e89d74e5c048ef
Gruzjan <gracjan122@protonmail.com>
2023-07-24 22:20:24 +0200

    basic DTOs

---
+++ chomer-backend/Models/DTO/HouseDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace chomer_backend.Models.DTO
+{
+    public class CreateHouseDTO
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(20, ErrorMessage = "Name must be between {2} and {1} characters.", MinimumLength = 1)]
+        public string Name { get; set; } = string.Empty;
+        [Required(ErrorMessage = "OwnerId is required.")]
+        public int OwnerId { get; set; }
+    }
+
+    public class HouseDTO
+    {
+        [Required(ErrorMessage = "Id is required.")]
+        public int Id { get; set; }
+    }
+}
---
+++ chomer-backend/Models/DTO/HouseUserDTO.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace chomer_backend.Models.DTO
+{
+    public class HouseUserDTO : CreateHouseUserDTO
+    {
+        [Required(ErrorMessage = "Id is required.")]
+        public int Id { get; set; }
+
+    }
+
+    public class CreateHouseUserDTO
+    {
+        [Required(ErrorMessage = "Points are required.")]
+        [Range(0, int.MaxValue, ErrorMessage = "Points must be positive.")]
+        public int Points { get; set; }
+        [Required(ErrorMessage = "UserId is required.")]
+        public int UserId { get; set; }
+        [Required(ErrorMessage = "HouseId is required.")]
+        public int HouseId { get; set; }
+        [Required(ErrorMessage = "Bool IsAdmin is required.")]
+        public bool IsAdmin { get; set; } = false;
+    }
+}
